152a2ee7360ed9bae37b3060f21e2a13b7de6f4f
Hannah Wood <hannahwood@Hannahs-MacBook-Pro.local>
2020-02-16 20:42:46 -0400

    I hate this

--- src/wesisdrivingmecrazy.java
+++ src/wesisdrivingmecrazy.java
@@ -4,7 +4,7 @@ public class wesisdrivingmecrazy {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("I am not incompetent");
-System.out.prinln("Whatsup fucks");
+System.out.println("Whatsup fucks");
 	}
 
 }
